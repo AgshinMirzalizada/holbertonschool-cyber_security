451abfb2edc2c11b83e02cfbabc573d36cde651b
Your Name <you@example.com>
2026-01-18 02:55:06 -0500

    .

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -3,10 +3,5 @@
 #1. this part deletes "XOR" format from cipher
 input=$(echo "$1" | sed 's/{xor}//g')
 
-#2. this part decodes base64
-decoded_base64=$(echo "$input" | base64 -d)
-
-#3. this part XOR every symbol with "_" according to websphere
-result=$(echo -n "$decoded_base64" | perl -pe '$_ ^= "_" x length')
-
-echo "$result"
+#2. this part decodes base64 and XOR every symbol with "_" according to websphere
+echo -n "$input" | base64 -d | perl -pe '$_ ^= "_" x length'
