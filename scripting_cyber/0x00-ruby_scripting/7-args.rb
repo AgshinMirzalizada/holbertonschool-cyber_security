9b06d6157ebd9de5a328243fad8a28a16b54da99
Your Name <you@example.com>
2026-05-24 09:13:42 +0400

    nineth task

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,11 +1,10 @@
 def print_arguments
-  # Check if the ARGV array is empty
   if ARGV.empty?
     puts "No arguments provided."
   else
-    # Iterate through each argument with its 0-indexed position
-    ARGV.each_with_index do |arg, index|
-      puts "#{index + 1}. #{arg}"
+    puts "Arguments:"
+    ARGV.each do |arg|
+      puts arg
     end
   end
 end
